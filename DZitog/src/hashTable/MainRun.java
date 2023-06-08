d86d92f3d4a521846d9e439bcec72ede3e6efdab
Anton Kruglyakov <akrugs@mail.ru>
2023-06-08 10:03:24 +0500

    Itog

--- DZitog/src/hashTable/MainRun.java
+++ DZitog/src/hashTable/MainRun.java
@@ -4,7 +4,7 @@ public class MainRun {
         public static void main(String[] args) {
             ht outer = new ht();
             for (int i = 0; i < 32; i++) {
-                outer.put( i + 1, i);
+                outer.put( i * 2, i);
             }
             System.out.println(outer.get(30));
             outer.replays(30, 2318);
@@ -14,7 +14,7 @@ public class MainRun {
             
             System.out.println(outer.size());
             
-            System.out.println(outer.containsKey(31));
-            System.out.println(outer.containsValue(28));
+            System.out.println(outer.containsKey(30));
+            System.out.println(outer.containsValue(62));
         }
 }
